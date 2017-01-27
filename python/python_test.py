9c66b42e1c21ebfb0ef848bf386d8fbce2f1dfa7
sirish <sireesh_nitw@yahoo.co.in>
2017-01-27 08:52:34 -0800

    updated

--- python/python_test.py
+++ python/python_test.py
@@ -4,9 +4,11 @@
 # print in python: prints on new lines
 print "Hello World"
 print 'Same Hello world with single quotes'
+print 'python auto inserts new line'
 # prints in same line by using comma
-print "print in",
-print 'same line'
+print "Can avoid default newline using",
+print 'the comma operator'
+print 'extra new line\n'
 # use escape sequence 
 print 'print single \' quote with backspace'
 print "print single ' quote within double quotes"
@@ -14,6 +16,11 @@ print 'print backspace \\ using backspace character'
 # python puts space
 i=50
 print 'python space: i value is',i
+print 'inserts extra newline \n'
+
+
+print 'enter interpreter mode by typing python on terminal'
+print 'exit interpreter mode by typing ctrl+d\n'
 
 
 # use format to print
@@ -24,28 +31,40 @@ print '{0} is {1} years old'.format(name, age)
 print '{} is {} years old: without index'.format(name, age)
 # use format for a different format specifier
 print '{0:.3f}'.format(1.0/3)
+print ''
+
 
 # multiple logical statements in one physical line
 i = 5
 print 'i value is', i
 i=10; print 'single line: i value is', i
+print ''
+
 
 # + operator
 a = 'i'
 b = 'am'
 s = a + b
+print '+ operator on strings concatenates the result'
 print 'i+am=', s
+print ''
+
 
 # * operator
 a = 'am'
 print 'am*3=', a*3
+print ''
+
 
 # indentation
 print 'group of statements with same indentation forms a block'
 print 'python suggested indentation is 4 spaces'
 print 'unnecessary indentation gives error'
+print ''
 
-# if: observe colon ':' after each if condition including else
+
+# if
+# observe colon ':' after each if condition including else
 # there is no switch operator in python
 val = 1
 if val == 1:
@@ -65,9 +84,19 @@ elif val == 3:
 else:
     #new block
     print 'else block'
+print ''
+
 
 # while
+# needs to use : after condition
 # a while can have an optional else clause
+test = 1
+while test == 1:
+    print 'While condition requires colon :'
+    print 'While can have an optional else clause'
+    test+=1
+print ''
+
 
 # for 
 # a for can have an optional else clause
@@ -77,8 +106,12 @@ for i in range(0,5):
 # step count 2
 for i in range(0,5,2):
     print 'step 2: iteration cnt:', i
+print ''
+
 
 # break and continue operators used as C language
+print 'break and continue operator used as in C\n'
+
 
 # functions
 # functions are defined using the def keyword
@@ -88,6 +121,7 @@ def fn_hello():
 
 print 'call fn_hello:'
 fn_hello()
+
 
 # global
 # global keyword should be used to assign value to a global variable inside a function
@@ -105,7 +139,9 @@ print 'outside fn: val is', val
 
 # keyword arguments
 
+
 # variable number of arguments
+
 
 # documentation strings
 def find_max_doc(x, y):
@@ -128,19 +164,24 @@ for i in sys.argv:
 #prints cmd arg strings, not numbers
  print i
 
+
 # import my test module
 import test_module
 test_module.fn_module()
 print 'value from module', test_module.val_mod
+
 
 # import functions, variables from test module
 from test_module import fn_module, val_mod
 fn_module()
 print 'value from module', val_mod
 
+
 # dir function to list the fn's, classes, variables part of a module
 
+
 # packages
+
 
 # data structures
 
